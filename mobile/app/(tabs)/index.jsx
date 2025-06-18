b920b30a39064fdedde6691e1396bc895e860544
LAPTOP-EL67H2O2\ASUS <asadeoer1605@gmail.com>
2025-06-18 17:28:57 +0700

    minor ubah index

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState, useRef } from 'react';
 import { Text, View, Image, ScrollView, TouchableOpacity, SafeAreaView, useWindowDimensions } from 'react-native';
 import { styles } from '../../assets/styles/home.styles.js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import jwt_decode from 'jwt-decode';
 import { useRouter } from 'expo-router';
 import * as WebBrowser from 'expo-web-browser';
 import { Feather } from '@expo/vector-icons'; 
@@ -50,11 +49,6 @@ export default function Page() {
     setName('User');
   }
 };
-
-  const handleSignOut = async () => {
-    await AsyncStorage.removeItem('token');
-    router.replace('/(auth)/sign-in');
-  };
 
   const handleClosestGymPress = async () => {
     const url = 'https://www.google.com/search?q=closest+gym&sca_esv=8900ff206bef6d47&biw=1536&bih=695&tbm=lcl&ei=j5JRaOiaBqv34-EP2vWPiAY&oq=clostes&gs_lp=Eg1nd3Mtd2l6LWxvY2FsIgdjbG9zdGVzKgIIADILEAAYgAQYkQIYigUyBxAAGIAEGAoyBxAAGIAEGAoyBxAAGIAEGAoyBxAAGIAEGAoyBxAAGIAEGAoyBxAAGIAEGAoyBxAAGIAEGAoyBxAAGIAEGAoyBxAAGIAEGApInB9QAFiJE3AAeACQAQCYAWWgAcwEqgEDNi4xuAEDyAEA-AEBmAIHoALuBMICChAAGIAEGEMYigXCAggQABiABBixA8ICCxAAGIAEGLEDGIMBwgIFEAAYgATCAgsQABiABBixAxiKBZgDAJIHAzYuMaAHkyayBwM2LjG4B-4EwgcFMC4yLjXIBxs&sclient=gws-wiz-local#rlfi=hd:;si:;mv:[[-7.0154913,110.43120569999999],[-7.084403399999999,110.3838238]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:14';
